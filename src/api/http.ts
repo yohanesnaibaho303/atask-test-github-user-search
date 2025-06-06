e440435ecb9a6e78ccd0b17ce99726566bfa300a
yohanesnaibaho303 <yohanespratama303@gmail.com>
2025-06-06 12:20:23 +0700

    [feat] add interceptors for error handling

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const http = axios.create({
   baseURL: "https://api.github.com",
@@ -6,3 +6,21 @@ export const http = axios.create({
     Accept: "application/vnd.github.v3+json",
   },
 });
+
+http.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    const defaultMsg = "An unknown error occurred";
+
+    if (error.response) {
+      const msg = (error.response.data as any)?.message;
+      return Promise.reject(new Error(msg || defaultMsg));
+    }
+
+    if (error.request) {
+      return Promise.reject(new Error("No response from server"));
+    }
+
+    return Promise.reject(new Error(error.message || defaultMsg));
+  }
+);
